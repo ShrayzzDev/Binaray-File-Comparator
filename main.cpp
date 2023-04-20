4e98ad1e9f7e2a774dadb8f7941a8fa68c7a9801
renaud.beuret <renaud.beuret@etu.uca.fr>
2023-04-20 13:25:33 +0200

    Corrected a bug where the program would endian swap the first file, and not the second one. Now give the option to endian swap the files, seperatly or not

--- main.cpp
+++ main.cpp
@@ -13,7 +13,8 @@ void swapByteOrder(unsigned short & us)
 
 int main (void)
 {
-    string path1, path2, yes_or_not;
+    cout << sizeof(unsigned int) << endl;
+    string path1, path2, yes_or_not, ask_endian_swap1, ask_endian_swap2;
     cout << "Path of the first binary file: ";
     cin >> path1;
     cout << path1 << endl;
@@ -26,14 +27,29 @@ int main (void)
         cout << "Wrong input, Do you want it going through all the file ?: ";
         cin >> yes_or_not;
     }
-    
+
+    cout << "Do you want it to endian swap for the first file ?" << endl;
+    cin >> ask_endian_swap1; 
+    while (ask_endian_swap1 != "Yes" && ask_endian_swap1 != "yes" && ask_endian_swap1 != "y" && ask_endian_swap1 != "No" && ask_endian_swap1 != "no" && ask_endian_swap1 != "n")
+    {
+	cout << "Wrong input, Do you want it to endian swap for the first file ? " << endl;
+	cin >> ask_endian_swap1;
+	}
+
+    cout << "Do you want it to endian swap for the second file ?" << endl;
+    cin >> ask_endian_swap2; 
+    while (ask_endian_swap2 != "Yes" && ask_endian_swap2 != "yes" && ask_endian_swap2 != "y" && ask_endian_swap2 != "No" && ask_endian_swap2 != "no" && ask_endian_swap2 != "n")
+    {
+	cout << "Wrong input, Do you want it to endian swap for the second file ? " << endl;
+	cin >> ask_endian_swap2;
+	}
+
     ifstream first_file, second_file;
     first_file.open(path1, ios::in | ios::binary);
     if (! first_file.is_open()){
         cerr << "Error: file 1 won't open" << endl;
         exit(1);
     }
-
     second_file.open(path2, ios::in);
     if (! second_file.is_open()){
         cerr << "Error: file 2 won't open" << endl;
@@ -55,9 +71,16 @@ int main (void)
     while(! first_file.eof() && ! second_file.eof())
     {
         unsigned short first_bin, second_bin;
-        first_file.read((char *)&first_bin, sizeof(unsigned short));
-        swapByteOrder(first_bin);
+        first_file.read((char *)&first_bin, 1);
+        if (ask_endian_swap1 == "Yes" || ask_endian_swap1 == "yes" || ask_endian_swap1 == "y"){
+            swapByteOrder(first_bin);
+        }
+
         second_file.read((char *)&second_bin, sizeof(unsigned short));
+        if (ask_endian_swap2 == "Yes" || ask_endian_swap2 == "yes" || ask_endian_swap2 == "y"){
+            swapByteOrder(second_bin);
+        }
+
         if (first_bin != second_bin){
             cout << "At offset " << hex << (first_file.tellg() - 2) << " ( " << hex << (second_file.tellg() - 2)
             << " ) the byte is " << hex << first_bin << " in the first file, and " << hex << second_bin << " in the second one" << endl;
